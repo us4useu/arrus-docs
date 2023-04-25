831acacbfb2356aaaa843afb3c10c97d3f229804
us4us developer <dev@us4us.eu>
2023-04-25 09:23:54 +0200

    'Updated docs: Updated docs, win_x86_64 build 69, issued by: pjarosik, 2023-04-25 07:23'

--- releases/v0.8.0-dev/matlab/_static/documentation_options.js
+++ releases/v0.8.0-dev/matlab/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.0-dev20230424',
+    VERSION: '0.8.0-dev20230425',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
