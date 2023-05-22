8b0768edde88fbf364ccf63190ef8c323d852587
us4us developer <dev@us4us.eu>
2023-05-22 09:51:22 +0000

    'Updated docs: Updated docs, linux_x86_64 build 11, issued by: pjarosik, 2023-05-22 09:51'

--- releases/v0.9.0-dev/python/_static/documentation_options.js
+++ releases/v0.9.0-dev/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0-dev20230420',
+    VERSION: '0.9.0-dev20230522',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
